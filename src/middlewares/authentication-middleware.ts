f48446f52e124223eaeecfaf1dbd3e542fdeb954
Gulilil <juan.csantoso@gmail.com>
2023-11-16 14:06:11 +0700

    fix: for authentication

--- src/middlewares/authentication-middleware.ts
+++ src/middlewares/authentication-middleware.ts
@@ -36,6 +36,7 @@ export class AuthenticationMiddleware {
                 res.status(StatusCodes.UNAUTHORIZED).json({
                     message: ReasonPhrases.UNAUTHORIZED,
                 });
+                return;
             }
         };
     }
